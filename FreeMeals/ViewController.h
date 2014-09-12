5d947a8f292a0455bfa738c818873efed5e19433
Zach Whelchel <zach.whelchel@gmail.com>
2014-09-11 22:49:11 -0400

    Initial Commit

---
+++ FreeMeals/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FreeMeals
+//
+//  Created by Zach Whelchel on 9/11/14.
+//  Copyright (c) 2014 ChurchOf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FreeMeals/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FreeMeals
+//
+//  Created by Zach Whelchel on 9/11/14.
+//  Copyright (c) 2014 ChurchOf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
